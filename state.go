eac01fec44c3012bd47b7a3e309fd960b2c495ff
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2015-08-04 21:58:30 +0200

    add state writer/reader

---
+++ state.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/binary"
+	"os"
+)
+
+// State is a path to the lis state file
+type State string
+
+// Read value from stateFile
+func (s *State) Read() (uint16, error) {
+	file, err := os.Open(string(*s))
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	data := make([]byte, 2)
+
+	_, err = file.Read(data)
+	if err != nil {
+		return 0, err
+	}
+
+	return binary.BigEndian.Uint16(data), nil
+}
+
+// Write value to stateFile
+func (s *State) Write(value uint16) error {
+	file, err := os.OpenFile(string(*s), os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	data := make([]byte, 2)
+	binary.BigEndian.PutUint16(data, value)
+
+	_, err = file.Write(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ state_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestWrite(t *testing.T) {
+	state := State("test")
+
+	err := state.Write(100)
+	if err != nil {
+		t.Errorf("failed to write state: %s", err.Error())
+	}
+}
+
+func TestRead(t *testing.T) {
+	state := State("test")
+
+	v, err := state.Read()
+	if err != nil {
+		t.Errorf("should not cause error")
+	}
+
+	if v != 100 {
+		t.Errorf("should be %d, was %d", 100, v)
+	}
+
+	err = os.Remove("test")
+	if err != nil {
+		t.Errorf("error when removing test state file")
+	}
+}
